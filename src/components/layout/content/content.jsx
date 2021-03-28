411de8987ec1767a37a75237650d5b45ad15e9c2
guilhermehilario <guilhermehilariosilva@gmail.com>
2021-03-28 08:59:25 -0300

    Adicionando rotas

--- src/components/layout/content/content.jsx
+++ src/components/layout/content/content.jsx
@@ -5,6 +5,7 @@ import React from "react";
 export default (props) => {
   const size = props.size;
   const color = props.color;
+  const image = props.image
 
   return <section className={`content color`}>{props.children}</section>;
 };
--- src/main/routes.jsx
+++ src/main/routes.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { Switch, Route } from "react-router";
 
 import About from "../components/pages/About";
+import Help from "../components/pages/Help";
+import Partner from "../components/pages/Partner";
+import Login from "../components/pages/Login";
+import Register from "../components/pages/Register";
 
 export default (props) => (
   <div className="">
