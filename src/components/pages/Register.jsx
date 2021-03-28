7f1183ad9c3c59d621ab0961d233206b6a24c9e7
Francisco Wallison <franciscowallison@gmail.com>
2021-03-28 15:45:58 -0300

    Iniciando Style component

--- src/components/pages/Register.jsx
+++ src/components/pages/Register.jsx
@@ -2,85 +2,92 @@ import React from "react";
 import Input from "../templates/input/input";
 import Button from "../templates/button/button";
 import Label from "../templates/label/label";
+import StyleRegister from "../../styles/Register.style";
 
 export default (props) => (
-  <section style={props}>
+  <div style={StyleRegister.container}>    
+  <section style={StyleRegister.information}>
+    <div style={StyleRegister.containerInfor}>
+      <img src="assets/InforTelaCadastro.png" alt="Italian Trulli" width="800" height="800"/> 
+    </div>    
+  </section>
+  <section style={StyleRegister.formRegistre}>
     <div style={props}>
-      <h1 style={props}> Quero aprender e ficar atualizado(a). </h1>
+      <h1 style={StyleRegister.formTitle}> Quero aprender e ficar atualizado(a). </h1>
     </div>
-    <form style={props}>
+    <form style={StyleRegister.form}>
       <Input
-        style={props}
+        style={StyleRegister.inputCadastro100}
         id="name"
         name="name"
         type="text"
         placeholder="Nome completo"
       />
       <Input
-        style={props}
+        style={StyleRegister.inputCadastro40}
         id="cpf"
         name="cpf"
-        type="number"
+        type="text"
         placeholder="CPF"
       />
       <Input
-        style={props}
+        style={StyleRegister.inputCadastro55}
         id="fone"
         name="fone"
-        type="number"
+        type="text"
         placeholder="Telefone"
       />
       <Input
-        style={props}
+        style={StyleRegister.inputCadastro40}
         id="state"
         name="state"
         type="text"
         placeholder="Estado"
       />
       <Input
-        style={props}
+        style={StyleRegister.inputCadastro55}
         id="cep"
         name="cep"
         type="number"
         placeholder="CEP"
       />
       <Input
-        style={props}
+        style={StyleRegister.inputCadastro45}
         id="city"
         name="city"
         type="text"
         placeholder="Cidade"
       />
       <Input
-        style={props}
+        style={StyleRegister.inputCadastro50}
         id="district"
         name="district"
         type="text"
         placeholder="BAIRRO"
       />
       <Input
-        style={props}
+        style={StyleRegister.inputCadastro100}
         id="address"
         name="address"
         type="text"
         placeholder="Endereço"
       />
       <Input
-        style={props}
+        style={StyleRegister.inputCadastro100}
         id="email"
         name="email"
         type="email"
         placeholder="Email"
       />
       <Input
-        style={props}
+        style={StyleRegister.inputCadastro45}
         id="password"
         name="password"
         type="password"
         placeholder="Senha"
       />
       <Input
-        style={props}
+        style={StyleRegister.inputCadastro45s}
         id="passwordConfirm"
         name="passwordConfirm"
         type="password"
@@ -97,4 +104,6 @@ export default (props) => (
       </div>
     </form>
   </section>
+  </div>
+  
 );
