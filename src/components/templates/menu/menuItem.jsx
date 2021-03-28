416a4a94d0dc277b90f3230e6d9ac4de0131a664
Francisco Wallison <franciscowallison@gmail.com>
2021-03-28 12:51:47 -0300

    css menuitem like

--- src/components/templates/menu/menuItem.jsx
+++ src/components/templates/menu/menuItem.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default (props) => (
   <li style={props.styles}>
-    <a href={props.path} style={props.styles} >
+    <a href={props.path} style={props.stylesLink} >
       <i className={props.icon}></i> {props.label}
     </a>
   </li>
